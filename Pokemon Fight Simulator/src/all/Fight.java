2e37a53b2f80b0be8b710eb50060d81ac6575d61
BloodLantern <BloodLantern221@gmail.com>
2021-04-30 16:17:56 +0200

    Modified Main and Fight class

--- Pokemon Fight Simulator/src/all/Fight.java
+++ Pokemon Fight Simulator/src/all/Fight.java
@@ -1,9 +1,39 @@
 package all;
 
+import pokemons.Pokemon;
+
 public class Fight {
-
-	public Fight() {
-		// TODO Auto-generated constructor stub
+	
+	Pokemon pokemon1;
+	Pokemon pokemon2;
+	
+	public Fight(Pokemon pokemon1, Pokemon pokemon2) {
+		
+		this.pokemon1 = pokemon1;
+		this.pokemon2 = pokemon2;
+		
+		System.out.println("Preparing a new fight between " + this.pokemon1.getName() + " and " + this.pokemon2.getName());
+		
 	}
-
+	
+	public void begin() {
+		
+		System.out.println("Beginning fight");
+		
+	}
+	
+	public Pokemon fastest(Pokemon pokemon1, Pokemon pokemon2) {
+		if (pokemon1.getSpeed() > pokemon2.getSpeed()) {
+			return pokemon1;
+		} else if (pokemon1.getSpeed() < pokemon2.getSpeed()) {
+			return pokemon2;
+		} else {
+			if (Math.random()*2 < 0.5) {
+				return pokemon1;
+			} else {
+				return pokemon2;
+			}
+		}
+	}
+	
 }
--- Pokemon Fight Simulator/src/all/Main.java
+++ Pokemon Fight Simulator/src/all/Main.java
@@ -1,10 +1,19 @@
 package all;
 
+import pokemons.Pokemon;
+import pokemons.electric.Pikachu;
+import pokemons.electric.Raichu;
+
 class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Pokemon pikachu = new Pikachu();
+		Pokemon raichu = new Raichu();
+		
+		Fight fight = new Fight(pikachu, raichu);
+		fight.begin();
+		
 	}
-
+	
 }
