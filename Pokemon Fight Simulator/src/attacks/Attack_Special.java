600138ee0732ce30b7d51437104684022d681b81
BloodLantern <maxime.gri.berlioz@gmail.com>
2021-05-03 18:33:03 +0200

    Updated Attack_Special class

--- Pokemon Fight Simulator/src/attacks/Attack_Special.java
+++ Pokemon Fight Simulator/src/attacks/Attack_Special.java
@@ -9,7 +9,7 @@ public enum Attack_Special implements Attacks {
 	private final Types type;
 	private final int power;
 	private final int accuracy;
-	private final int pp;
+	private int pp;
 	private final String secondaryEffect;
 	private final String textName;
 	
@@ -28,30 +28,58 @@ public enum Attack_Special implements Attacks {
 	 * pokemon attacker against the pokemon attacked. It returns
 	 * the final damage of this attack with these parameters.
 	 */
-	public int use(Attack_Special attack, Pokemon attacker, Pokemon attacked) {
-		double CM = 1;
-		
-		if (attack.getType() == attacker.getType())
-			CM *= 1.5;
-		
-		// TODO Critical hit
-		
-		double rdm = 1;
-		do {
-			rdm = Math.random();
-		} while (rdm < 0.85);
-		CM *= rdm;
-		
-		// TODO Objects, talents multiplier
-		
-		CM *= Pokemon.weakness(attacker.getType(), attacked.getType());
-		
-		// Damage formula
-		int finalDamage = (int)Math.floor(((attacker.getLevel()*0.4+2)*attacker.getSpAtk()*attack.getPower()/attacked.getSpDef()*50+2)*CM);
-		
-		attacked.setHP(attacked.getHP() - finalDamage);
-		
-		return finalDamage;
+	public static int use(Attack_Special attack, Pokemon attacker, Pokemon attacked) {
+		if (attack != null) {
+			
+			if (attack.getPp() > 0) {
+				attack.setPp(attack.getPp() - 1);
+				
+				double CM = 1;
+				
+				if (attack.getType() == attacker.getType())
+					CM *= 1.5;
+				
+				// TODO Critical hit
+				
+				double rdm = 1;
+				do {
+					rdm = Math.random();
+				} while (rdm < 0.85);
+				CM *= rdm;
+			
+				// TODO Objects, talents multiplier
+				
+				float weakness = 1.0f;
+				weakness *= Pokemon.weakness(attack.getType(), attacked.getType());
+				if (attacked.getType2() != null)
+					weakness *= Pokemon.weakness(attack.getType(), attacked.getType2());
+			
+				if (weakness < 0.5f) {
+					System.out.println("It isn't efficient at all !");
+				} else if (weakness >= 0.5f && weakness < 1.0f) {
+					System.out.println("It isn't very efficient.");
+				} else if (weakness >= 1.0f && weakness < 1.5f) {
+					System.out.println("It is efficient.");
+				} else if (weakness >= 1.5f) {
+					System.out.println("It is very efficient !");
+				}
+				CM *= weakness;
+				
+				// Damage formula
+				int finalDamage = (int)Math.floor((((attacker.getLevel()*0.4+2)*attacker.getSpAtk()*attack.getPower())/(attacked.getSpDef()*50)+2)*CM);
+				
+				attacked.setHP(Math.max(attacked.getHP() - finalDamage, 0));
+				
+				return finalDamage;
+			} else {
+				System.err.println("You don't have enough PP to use " + attack.getTextName() + " !");
+				return -1;
+			}
+			
+		}  else {
+			System.err.println("Attack is null !");
+			return -1;
+		}
 	}
 
 	public Types getType() {
@@ -68,6 +96,10 @@ public enum Attack_Special implements Attacks {
 
 	public int getPp() {
 		return pp;
+	}
+	
+	public void setPp(int pp) {
+		this.pp = pp;
 	}
 
 
