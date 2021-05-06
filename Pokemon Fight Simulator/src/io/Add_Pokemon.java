52cce7bdf902116d05be949ed3f1bfc6c6e208bc
BloodLantern <maxime.gri.berlioz@gmail.com>
2021-05-06 12:52:19 +0200

    Fixed Add_Pokemon class to be fully functional

--- Pokemon Fight Simulator/src/io/Add_Pokemon.java
+++ Pokemon Fight Simulator/src/io/Add_Pokemon.java
@@ -20,17 +20,23 @@ public abstract class Add_Pokemon {
 		
 		String name;
 		Types type;
+		Types type2 = null;
 		int hp;
 		int atk;
 		int def;
 		int spatk;
 		int spdef;
 		int speed;
+		String evolve;
 		
 		System.out.print("Please input the following parameter:\nName: ");
 		name = params.nextLine();
 		System.out.print("\nType: ");
 		type = Types.valueOf(params.nextLine().toUpperCase());
+		System.out.print("\nType 2 (leave blank if not): ");
+		String temp = params.nextLine();
+		if (temp != "")
+			type2 = Types.valueOf(temp.toUpperCase());
 		System.out.print("\nHPs: ");
 		hp = params.nextInt();
 		System.out.print("\nAtk: ");
@@ -43,10 +49,17 @@ public abstract class Add_Pokemon {
 		spdef = params.nextInt();
 		System.out.print("\nSpeed: ");
 		speed = params.nextInt();
+		System.out.print("\nEvolves from (write 'Pokemon' if not): ");
+		params.nextLine();
+		evolve = params.nextLine();
 
 		params.close();
 		
-		System.out.println("\nCreating new Pokemon " + name + " with stats:\nHPs: " + hp + "\nAtk: " + atk + "\nDef: " + def + "\nSpAtk: " + spatk + "\nSpDef: " + spdef + "\nSpeed: " + speed);
+		if (temp != "") {
+			System.out.println("\nCreating new Pokemon " + name + " of type " + type.toString() + "-" + type2.toString() + " evolving from " + evolve + " with stats:\nHPs: " + hp + "\nAtk: " + atk + "\nDef: " + def + "\nSpAtk: " + spatk + "\nSpDef: " + spdef + "\nSpeed: " + speed);
+		} else {
+			System.out.println("\nCreating new Pokemon " + name + " of type " + type.toString() + " evolving from " + evolve + " with stats:\nHPs: " + hp + "\nAtk: " + atk + "\nDef: " + def + "\nSpAtk: " + spatk + "\nSpDef: " + spdef + "\nSpeed: " + speed);
+		}
 		
 		file = new File("src\\pokemons\\" + type.toString().toLowerCase() + "\\" + name + ".java");
 		
@@ -70,7 +83,7 @@ public abstract class Add_Pokemon {
 					+ "import pokemons.Pokemon;\r\n"
 					+ "import pokemons.Types;\r\n"
 					+ "\r\n"
-					+ "public class " + name + " extends Pokemon {\r\n"
+					+ "public class " + name + " extends " + evolve + " {\r\n"
 					+ "\r\n"
 					+ "public " + name + "() {\r\n"
 					+ "		\r\n"
@@ -108,8 +121,10 @@ public abstract class Add_Pokemon {
 					+ "	\r\n"
 					+ "	@Override\r\n"
 					+ "	protected void initStats() {\r\n"
-					+ "		this.setType(Types." + type + ");\r\n"
-					+ "		this.setBaseHP(" + hp + ");\r\n"
+					+ "		this.setType(Types." + type + ");\r\n");
+			if (temp != "")
+				writer.write("		this.setType2(Types." + type2 + ");\r\n");
+			writer.write("		this.setBaseHP(" + hp + ");\r\n"
 					+ "		this.setBaseAtk(" + atk + ");\r\n"
 					+ "		this.setBaseDef(" + def +");\r\n"
 					+ "		this.setBaseSpAtk(" + spatk + ");\r\n"
@@ -140,7 +155,7 @@ public abstract class Add_Pokemon {
 		}
 
 		if (operationSuccess) {
-			System.out.println("New pokemon file created ! (" + (System.currentTimeMillis() - timer) + "ms)\nYou may need to refresh the " + type.toString() + " package");
+			System.out.println("New pokemon file created at '" + file.getAbsolutePath() + "' ! (" + (System.currentTimeMillis() - timer) + "ms)\nYou may need to refresh the " + type.toString() + " package");
 		} else {
 			System.err.println("An error has occured :(");
 		}
