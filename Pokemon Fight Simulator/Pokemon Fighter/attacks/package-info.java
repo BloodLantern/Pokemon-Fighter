4cd7fc11a24c522ce476db75e8aaf24162e9a28f
BloodLantern <maxime.gri.berlioz@gmail.com>
2021-05-14 12:47:42 +0200

    Added package-info for each package

---
+++ Pokemon Fight Simulator/Pokemon Fighter/attacks/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains all attack types.
+ */
+package attacks;
---
+++ Pokemon Fight Simulator/Pokemon Fighter/io/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all classes about IO (input/output streams). It is all
+ * file-writing/reading classes.
+ */
+package io;
